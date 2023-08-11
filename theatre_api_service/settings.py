28765814ebbcef638185104cf8d37c1365a55f1a
maccarets <maccarets@gmail.com>
2023-08-11 18:57:01 +0300

    change JWT settings

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
